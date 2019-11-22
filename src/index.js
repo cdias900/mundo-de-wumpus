0e916551728135ec0e8860aca8eaed3823f2a54d
cdias900 <cdias900@gmail.com>
2019-11-21 23:29:25 -0300

    Adiciona ServiceWorker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 // Área de importação dos componentes que serão utilizados na renderização em tela
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 import './index.css';
 import App from './App';
 //
 
 // Adiciona todo o código na página html
 ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.register();
